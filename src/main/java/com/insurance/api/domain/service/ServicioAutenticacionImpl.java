76986c92afe2212815623aa3ca99843557e93267
cuentacompartida1895 <cuentacompartida1895@gmail.com>
2025-09-14 21:47:05 +0000

    Ajuste por no ejecucion

---
+++ src/main/java/com/insurance/api/domain/service/ServicioAutenticacionImpl.java
@@ -0,0 +1,43 @@
+package com.insurance.api.domain.service;
+
+import com.insurance.api.domain.model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@Service
+public class ServicioAutenticacionImpl implements ServicioAutenticacion {
+
+    private final JwtEncoder encoder;
+    private final UserDetailsService userDetailsService;
+
+    public ServicioAutenticacionImpl(JwtEncoder encoder, UserDetailsService userDetailsService) {
+        this.encoder = encoder;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    public String login(Usuario usuario) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(usuario.getUsername());
+        Instant now = Instant.now();
+        long expiry = 36000L;
+        String scope = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(userDetails.getUsername())
+                .claim("scope", scope)
+                .build();
+        return this.encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+}
--- src/main/java/com/insurance/api/infrastructure/config/ConfiguracionAplicacion.java
+++ src/main/java/com/insurance/api/infrastructure/config/ConfiguracionAplicacion.java
@@ -2,8 +2,11 @@ package com.insurance.api.infrastructure.config;
 
 import com.insurance.api.application.usecase.AutenticarUsuario;
 import com.insurance.api.domain.service.ServicioAutenticacion;
+import com.insurance.api.domain.service.ServicioAutenticacionImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 
 @Configuration
 public class ConfiguracionAplicacion {
@@ -11,5 +14,10 @@ public class ConfiguracionAplicacion {
     @Bean
     public AutenticarUsuario autenticarUsuario(ServicioAutenticacion servicioAutenticacion) {
         return new AutenticarUsuario(servicioAutenticacion);
+    }
+
+    @Bean
+    public ServicioAutenticacion servicioAutenticacion(JwtEncoder encoder, UserDetailsService userDetailsService) {
+        return new ServicioAutenticacionImpl(encoder, userDetailsService);
     }
 }
